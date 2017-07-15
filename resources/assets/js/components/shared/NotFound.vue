c7b91af1db12b4e64e996036ea0b8c2639de18c9
nigamshirish <nigamshirish@gmail.com>
2017-07-15 20:56:14 +1000

    Fix text positioning in NotFound Component

--- resources/assets/js/components/shared/NotFound.vue
+++ resources/assets/js/components/shared/NotFound.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="404">
+<div class="container">
+  <div class="text-center not-found">
     <h1>404</h1>
     <h4>This page does not exist or moved to anothe location</h4>
+  </div>
   </div>
 </template>
 <script>
   export default {}
 </script>
 <style>
-  .404 {
-    height:100vh;
+  .not-found {
+    padding-top:100px;
   }
 </style>
